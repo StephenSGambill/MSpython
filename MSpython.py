c27eda0af39b1513e1d432cb9b94b562496f331d
Stephen Gambill <stephensgambill@gmail.com>
2023-04-04 13:57:54 -0500

    Initial commit

---
+++ MSpython.py
@@ -0,0 +1,45 @@
+from datetime import date  # import date module
+import sys
+
+# print('Hello, World!')
+
+# sum = 1 + 2
+# print(sum)
+# sum = 1 + 2  # 3
+# product = sum * 2
+# print(product)
+# product *= 3  # also =, +=, -=, /=, *=
+# print(product)
+
+# # boolean value must be capitalized - True, False
+
+
+# when used with command line python3 pythonlearn.py 2023-01-01 prints full array with two arguments, then file name, then date from command line
+# print(sys.argv)
+# print(sys.argv[0])
+# print(sys.argv[1])
+
+# print("Welcome to the greeter program")
+# name = input("Enter your name: ")
+# print("Greetings " + name)
+
+
+# parsecs = 11
+# lightyears = parsecs * 3.26
+# print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
+
+
+# print(date.today())
+# print("Today's date is: " + str(date.today()))  # must convert to string
+# interpolation is accompythplished with + for strings
+
+
+# receiving input
+# print("calculator program")
+# first_number = input("first number: ")
+# second_number = input("second number: ")
+# print(first_number + second_number)  # prints as two strings together
+# print(int(first_number) + int(second_number))  # prints as calculated number
+
+# activate virtual environment - source env/bin/activate (result is (env) in terminal prompt)
+# deactivate virtual environment - deactivate
